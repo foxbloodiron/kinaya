5b5dca724cfe68d19e47d9865610035109da2db4
foxbloodiron <ariakbar6944@gmail.com>
2018-07-24 09:52:28 +0700

    js cookie

--- resources/views/layouts/_script.blade.php
+++ resources/views/layouts/_script.blade.php
@@ -28,6 +28,7 @@
   <!-- End custom js for this page-->
 
   <script src="{{asset('assets/datatables/datatables.min.js')}}"></script>
+  <script src="{{asset('assets/js-cookie/js.cookie.js')}}"></script>
   
   <script type="text/javascript">
   $(document).ready(function(){
@@ -57,4 +58,116 @@
       format:"dd-mm-yyyy"
     });
   });
+
+    // Get Cookie
+    var sidebar = Cookies.get('sidebar_kinaya');
+
+    // Cookie Sidebar Exists
+    if (sidebar){
+        $('body').addClass(sidebar);
+
+        if (sidebar=='sidebar-light') 
+        {
+          $('#sidebar-light-theme').addClass('selected');
+          $('#sidebar-default-theme').removeClass('selected');
+
+        }
+        if (sidebar=='sidebar-default') 
+        {
+          $('#sidebar-default-theme').addClass('selected');
+          $('#sidebar-light-theme').removeClass('selected');
+
+        }
+    }
+    // Cookie Sidebar Doesn't Exist
+    else {
+        $('body').addClass();
+    }
+
+    // Sidebar Option Cookie
+    $('#sidebar-light-theme').on('click', function(){
+        $('body').addClass('sidebar-light');
+        $('#sidebar-light-theme').addClass('sidebar-light selected');
+        Cookies.set('sidebar_kinaya', 'sidebar-light',{ expires : 365});
+
+    });
+
+    $('#sidebar-default-theme').on('click', function(){
+        $('body').removeClass('sidebar-light');
+        Cookies.set('sidebar_kinaya', 'sidebar-default',{ expires : 365});
+    });
+
+    // Get Cookie
+    var navbar  = Cookies.get('navbar_kinaya');
+    // Cookie Navbar Exists
+    if (navbar){
+        $('.navbar').addClass(navbar);
+        if(navbar=='navbar-primary')
+        {
+          $('div.tiles.primary').addClass('selected');
+        }
+        if(navbar=='navbar-success')
+        {
+          $('div.tiles.success').addClass('selected');
+        }
+        if(navbar=='navbar-warning')
+        {
+          $('div.tiles.warning').addClass('selected');
+        }
+        if(navbar=='navbar-danger')
+        {
+          $('div.tiles.danger').addClass('selected');
+        }
+        if(navbar=='navbar-pink')
+        {
+          $('div.tiles.pink').addClass('selected');
+        }
+        if(navbar=='navbar-dark')
+        {
+          $('div.tiles.dark').addClass('selected');
+        }
+        if(navbar=='navbar-light')
+        {
+          $('div.tiles.light').addClass('selected');
+        }
+    }
+    // Cookie Navbar Doesn't Exist
+    else {
+        $('.navbar').addClass('navbar-light');
+        $('div.tiles.light').addClass('selected');
+    }
+    // Navbar Option Cookie
+    $('div.tiles.primary').on('click', function(){
+        $('.navbar').addClass('navbar-primary');
+        Cookies.set('navbar_kinaya', 'navbar-primary', {expires : 365});
+    });
+    $('div.tiles.success').on('click', function(){
+        $('.navbar').addClass('navbar-success');
+        Cookies.set('navbar_kinaya', 'navbar-success', {expires : 365});
+    });
+    $('div.tiles.warning').on('click', function(){
+        $('.navbar').addClass('navbar-warning');
+        Cookies.set('navbar_kinaya', 'navbar-warning', {expires : 365});
+    });
+    $('div.tiles.danger').on('click', function(){
+        $('.navbar').addClass('navbar-danger');
+        Cookies.set('navbar_kinaya', 'navbar-danger', {expires : 365});
+    });
+    $('div.tiles.pink').on('click', function(){
+        $('.navbar').addClass('navbar-pink');
+        Cookies.set('navbar_kinaya', 'navbar-pink', {expires : 365});
+    });
+    $('div.tiles.info').on('click', function(){
+        $('.navbar').addClass('navbar-info');
+        Cookies.set('navbar_kinaya', 'navbar-info', {expires : 365});
+    });
+    $('div.tiles.dark').on('click', function(){
+        $('.navbar').addClass('navbar-dark');
+        Cookies.set('navbar_kinaya', 'navbar-dark', {expires : 365});
+    });
+    $('div.tiles.light').on('click', function(){
+        $('.navbar').addClass('navbar-light');
+        Cookies.set('navbar_kinaya', 'navbar-light', {expires : 365});
+    });
+
 </script>
